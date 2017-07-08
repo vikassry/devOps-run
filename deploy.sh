b3cbdb8f8c6221a5d2f8abff56c4799f6751b5d8
vikassry <vikassry@users.noreply.github.com>
2017-07-08 18:01:46 +0530

    fixing update task

--- deploy.sh
+++ deploy.sh
@@ -13,4 +13,4 @@ aws s3 cp build/libs/hello-boot-0.1.0.jar.gz s3://spring-boot-jeetu/application.
 aws elasticbeanstalk create-application-version --application-name "My First Elastic Beanstalk Application" --version-label $TRAVIS_BUILD_NUMBER --description application-v1 --source-bundle S3Bucket="spring-boot-jeetu",S3Key="application.jar.zip"
 
 #update environment with the newly created version
-aws elasticbeanstalk update-environment --application-name "My First Elastic Beanstalk Application" --version-label $TRAVIS_BUILD_NUMBER —environment-name Default-Environment
+aws elasticbeanstalk update-environment --application-name "My First Elastic Beanstalk Application" --version-label $TRAVIS_BUILD_NUMBER --environment-name "Default-Environment"
