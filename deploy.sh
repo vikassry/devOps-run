a9e6c855cc330cacfd38b92b758af5587a3e012f
vikassry <vikassry@users.noreply.github.com>
2017-07-08 17:54:38 +0530

    installing aws clip

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+#install aws cli
+pip install awscli --upgrade --user
+
 # zip the jar artifact
 gzip build/libs/hello-boot-0.1.0.jar
 
@@ -7,7 +10,7 @@ gzip build/libs/hello-boot-0.1.0.jar
 aws s3 cp build/libs/hello-boot-0.1.0.jar.gz s3://spring-boot-jeetu/application.jar.zip
 
 # create new version from of the application
-aws elasticbeanstalk create-application-version --application-name "My First Elastic Beanstalk Application" --version-label v1 --description application-v1 --source-bundle S3Bucket="spring-boot-jeetu",S3Key="application.jar.zip"
+aws elasticbeanstalk create-application-version --application-name "My First Elastic Beanstalk Application" --version-label $TRAVIS_BUILD_NUMBER --description application-v1 --source-bundle S3Bucket="spring-boot-jeetu",S3Key="application.jar.zip"
 
 #update environment with the newly created version
 aws elasticbeanstalk update-environment --application-name "My First Elastic Beanstalk Application" --version-label $TRAVIS_BUILD_NUMBER —environment-name Default-Environment
