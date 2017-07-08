538f2d2365a1a9908bc8e6b25c8b5d2bf89c4502
vikassry <vikassry@users.noreply.github.com>
2017-07-08 18:47:15 +0530

    caching pip and java

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ pip install awscli --upgrade --user
 aws s3 cp build/distributions/bootDemo-1.0-SNAPSHOT.zip s3://spring-boot-jeetu/bootDemo-$TRAVIS_BUILD_NUMBER.zip
 
 # create new version from of the application
-aws elasticbeanstalk create-application-version --application-name "My First Elastic Beanstalk Application" --version-label $TRAVIS_BUILD_NUMBER --description application-v1 --source-bundle S3Bucket="spring-boot-jeetu",S3Key="bootDemo-$TRAVIS_BUILD_NUMBER.zip"
+aws elasticbeanstalk create-application-version --application-name "My First Elastic Beanstalk Application" --version-label bootDemo-$TRAVIS_BUILD_NUMBER.zip --description application-v1 --source-bundle S3Bucket="spring-boot-jeetu",S3Key="bootDemo-$TRAVIS_BUILD_NUMBER.zip"
 
 #update environment with the newly created version
 aws elasticbeanstalk update-environment --application-name "My First Elastic Beanstalk Application" --version-label bootDemo-$TRAVIS_BUILD_NUMBER.zip --environment-name "Default-Environment"
