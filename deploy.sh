4eb9507e58f52dbd429cd7974d7eb14c01a726ef
vikassry <vikassry@users.noreply.github.com>
2017-07-08 18:39:40 +0530

    Refactor | adding Procfile

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,11 @@
 #install aws cli
 pip install awscli --upgrade --user
 
-# zip the jar artifact
-gzip build/libs/hello-boot-0.1.0.jar
-
 # upload the artifact to s3 bucket
-aws s3 cp build/libs/hello-boot-0.1.0.jar.gz s3://spring-boot-jeetu/application.jar.zip
+aws s3 cp build/distributions/bootDemo-1.0-SNAPSHOT.zip s3://spring-boot-jeetu/bootDemo-$TRAVIS_BUILD_NUMBER.zip
 
 # create new version from of the application
-aws elasticbeanstalk create-application-version --application-name "My First Elastic Beanstalk Application" --version-label $TRAVIS_BUILD_NUMBER --description application-v1 --source-bundle S3Bucket="spring-boot-jeetu",S3Key="application.jar.zip"
+aws elasticbeanstalk create-application-version --application-name "My First Elastic Beanstalk Application" --version-label $TRAVIS_BUILD_NUMBER --description application-v1 --source-bundle S3Bucket="spring-boot-jeetu",S3Key="bootDemo-$TRAVIS_BUILD_NUMBER.zip"
 
 #update environment with the newly created version
-aws elasticbeanstalk update-environment --application-name "My First Elastic Beanstalk Application" --version-label $TRAVIS_BUILD_NUMBER --environment-name "Default-Environment"
+aws elasticbeanstalk update-environment --application-name "My First Elastic Beanstalk Application" --version-label bootDemo-$TRAVIS_BUILD_NUMBER.zip --environment-name "Default-Environment"
